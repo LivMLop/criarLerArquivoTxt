6d0f3effe4dbccd5c38a7424dc3f837ce9f7720f
LivMLop <96291523+LivMLop@users.noreply.github.com>
2022-01-05 16:52:28 +0000

    Add files via upload

---
+++ criarLerArquivo.py
@@ -0,0 +1,26 @@
+with open("Os Lusíadas.txt", "x") as f:
+    f.write(''' “As armas e os barões assinalados
+
+Que, da ocidental praia lusitana,
+
+Por mares nunca de antes navegados
+
+Passaram ainda além da Taprobana,
+
+Em perigos e guerras esforçados,
+
+Mais do que prometia a força humana,
+
+E entre gente remota edificaram
+
+Novo reino, que tanto sublimaram. ”
+
+Os Lusíadas, Canto I, estrofe 1''')
+
+with open("Os Lusíadas.txt", "r") as f:
+    txt = f.read()
+    print(txt)
+
+
+
+
